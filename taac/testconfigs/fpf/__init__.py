d5d890f3c0ebebfc57423ca86252ad00f3915f7c
generatedunixname89002005307016 <generatedunixname89002005307016@meta.com>
2026-06-08 12:16:02 -0700

    Differential Revision: D107852474
    fbshipit-source-id: 17b613396607fa6162c49a0037a6cccd0501cff3

--- taac/testconfigs/fpf/__init__.py
+++ taac/testconfigs/fpf/__init__.py
@@ -1,5 +1,7 @@
 # (c) Meta Platforms, Inc. and affiliates. Confidential and proprietary.
 
+# pyre-unsafe
+
 from taac.testconfigs.fpf.fpf_stress_test_config import (
     TEST_CONFIG as FPF_STRESS_TEST_CONFIG,
 )
